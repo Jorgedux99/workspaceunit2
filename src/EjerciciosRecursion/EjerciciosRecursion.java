eb9b71697a51d112bfe4d0619e5d3feef023544f
Jorgedux99 <jirgeserramo9@gmail.com>
2024-01-12 14:02:58 +0100

    12/01/24

--- src/EjerciciosRecursion/EjerciciosRecursion.java
+++ src/EjerciciosRecursion/EjerciciosRecursion.java
@@ -41,8 +41,52 @@ public class EjerciciosRecursion {
 			
 		}
 		
+		private static int reverseNumberAux(int n, int result) {
+			
+			int digit = 0;
+			
+			if (n==0) {
+				return result;
+			} else {
+
+				digit = n % 10;
+				
+				result = digit + result * 10;
+				
+				n = n/10;
+				return reverseNumberAux(n,result);				
+			}
+		}
+	
+		public static int sumaNPrimerosNumerosAux (int n, int acumulador) {
+			
+			if (n==0)
+				return acumulador;
+			else 
+				return sumaNPrimerosNumerosAux(n-1, acumulador+n);
+			
+		}
 		
-	}
+		public static int sumaNPrimerosNumerosMejorado (int n) {
+			
+			
+			return sumaNPrimerosNumerosAux(n,0);
+			
+			
+			
+		}
+		
+		public static int terminoFibonacci (int n) {
+			
+			if (n==0 || n==1) {
+				
+				return 1;
+			} else {
+				
+				return terminoFibonacci(n-1) + terminoFibonacci (n-2);
+			}
+			
+		}
 	
 	public static void main(String[] args) {
 		
